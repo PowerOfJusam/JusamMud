8df10c6a921d80b9a5de05d6af75a39f94986adb
Jaiven17 <jaiven@gmail.com>
2017-04-15 16:16:17 -0500

    updated

--- LoginIdea.py
+++ LoginIdea.py
@@ -1,24 +1,24 @@
-#The function to handle adding new users to accounts.txt which repeats if name is under 2 characters and checks to see if all alpha with no spaces.
+
 useraccount = open('accounts.txt', 'r+')
+registeredusers = useraccount.read()
+print registeredusers
+
+#The function to handle adding new users to accounts.txt which repeats if name is under 2 characters and checks to see if all alpha with no spaces.
 def getnewuser():
     tempname = raw_input('Please name your new character.\n')
-    if len(tempname) > 2 and len(tempname) < 20 and tempname.isalpha() and not tempname.isspace():
-            for line in useraccount:
-                # all lines that start with From: it prints
-                if line.startswith(tempname):
-                    print "Sorry, that name is already taken. try again"
-                    tempname = None
-                    getnewuser()
-                else:
-                    useraccount.write(tempname.capitalize())
+    tempname.capitalize()
+    if len(tempname) > 2 and len(tempname) < 20 and tempname.isalpha() not in registeredusers:
+        useraccount.write(tempname)
     else:
         print "Name must be greater then two characters, less then 20 characters, contain no numbers, symbols or spaces.\n"
+        tempname = None
         getnewuser()
 
 #Character login where you get the option to enter an already created character or type create to run the function getnewuser
 name = raw_input("Please enter a name or type 'create' to create a new character\n")
 if name.lower() == 'create':
     getnewuser()
+
 #loads name file.
 #searches for name and if name isn't there writes name to file on next line
 #prompts for a password
