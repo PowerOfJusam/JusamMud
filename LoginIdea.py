b2ab3ef835d3a22be5fd2f3caaac00033d35fbe0
Jaiven17 <jaiven@gmail.com>
2017-04-16 07:58:21 -0500

    flusing out password definition

--- LoginIdea.py
+++ LoginIdea.py
@@ -6,13 +6,14 @@ def getnewuser():
     useraccount = open('accounts.txt', 'r')
     registeredusers = useraccount.read().split('\n')
     useraccount.close()
-
+    #namePassed used to prompt for new password if the new player named passed and finished transferring into accounts.txt file.
+    namePassed = False
     nameExists = False
     tempname = raw_input('Please name your new character.\n')
     tempname = tempname.capitalize()
     #iterate through each element and check to see if the user already exists
     for user in registeredusers:
-        if user == tempname:
+        if user == tempname + ':':
             nameExists = True
             break
     #if the length is between 2 and 20
@@ -23,19 +24,48 @@ def getnewuser():
             if nameExists == False:
                 #searches for name and if name isn't there writes name to file on next line
                 useraccount = open('accounts.txt','a')
-                useraccount.write(tempname + '\n')
+                #adds the : between name and password for later file searching to verify user accounts.
+                useraccount.write(tempname + ":" + '\n')
                 useraccount.close()
+                namePassed = True
+                print "Welcome to Jusam Mud " + tempname
             else:
                 #the name exists, so let our user know
                 print "Name exists. Please try another name."
+                namePassed = False
                 getnewuser()
         else:
             print "The name may only contain characters."
+            namePassed = False
             getnewuser()
     else:
         print "Name must be greater then two characters, less then 20 characters."
+        namePassed = False
         getnewuser()
-
+    if namePassed == True:
+        getnewpassword(tempname)
+def getnewpassword(tempname):
+    passwordPassed = False
+    passwordComplete = False
+    newpassword = raw_input('Please enter a password for ' + tempname + '.' + ' Please note that you cannot use spaces when creating a password.\n')
+    if ' ' in newpassword:
+        print 'Sorry but your password has a space in it. Remember, you cannot have spaces in your password. Try again.\n'
+        getnewpassword(tempname)
+        passwordPassed = False
+    else:
+        print "Password accepted.\n"
+        passwordPassed = True
+    if passwordPassed == True:
+        retypedpassword = raw_input('Please enter your password again.\n')
+        if newpassword == retypedpassword:
+            print "User account created."
+            retypedpassword = setuserpassword
+            passwordComplete = True
+        else:
+            print "passwords don't match. Please try again"
+            getnewpassword(tempname)
+    if passwordComplete = True:
+        
 #Character login where you get the option to enter an already created character or type create to run the function getnewuser
 name = raw_input("Please enter a name or type 'create' to create a new character\n")
 if name.lower() == 'create':
@@ -49,7 +79,7 @@ if name.lower() == 'create':
 #if password = after tempname pw then creates character
 
 #login with a character already created
-#if name isn't create search file for name
+
 #if name found move to request password
 #load all text after name[:] search to variable password
 #prompt for password
